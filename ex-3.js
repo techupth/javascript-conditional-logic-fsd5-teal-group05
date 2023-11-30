0923753633801e21e190e6cdac318a78ce4a7c43
Krit <titatit.akkh@gmail.com>
2023-11-30 12:44:03 +0700

    Feat: Finished Ex1-7

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,10 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+let message = lightBulbStatus === "Off" 
+    ? "Light bulb is Off"
+    : lightBulbStatus === "On" 
+    ? "Light bulb is ON"
+    : "Please choose the correct input (On/Off)";
+
+console.log(message);
